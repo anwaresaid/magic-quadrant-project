fa9f123104a3b87e6153db4d01d1c4280d9e3c2d
anwaresaid <51533569+anwaresaid@users.noreply.github.com>
2021-09-20 01:44:54 +0300

    completed dogan_tech project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import Board from "./components/Board";
+import { React, useState, useEffect } from "react";
+import "./main.css";
 
 function App() {
+  const [dots, setDots] = useState([]);
+  const [i, setI] = useState(0);
+  const [rend, setRend] = useState(true);
+
+  const addCard = () => {
+    const dotscopy = Object.assign([], dots);
+    var cardId = "card-" + (i + 1);
+    setI(i + 1);
+    dotscopy.push({
+      id: cardId,
+      className: "card",
+      name: "new",
+      x_axis: 50,
+      y_axis: 50,
+    });
+    setDots([...dotscopy]);
+    if (dotscopy) localStorage.setItem("dots", JSON.stringify(dotscopy));
+    var x = JSON.stringify(dotscopy);
+  };
+  var temp = [];
+  useEffect(() => {
+    temp = localStorage.getItem("dots");
+    // temp = JSON.parse(temp);
+    //console.log("check", JSON.parse(temp) );
+    //if (temp) setDots(JSON.parse(temp));
+  }, []);
+
+  const deleteCard = (id) => {
+    let dotscopy = Object.assign([], dots);
+    let index = dotscopy.findIndex((dotcopy) => dotcopy.id === id);
+
+    dotscopy.splice(index, 1);
+    setDots([...dotscopy]);
+    if (dotscopy) localStorage.setItem("dots", JSON.stringify(dotscopy));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <main className='flexbox'>
+        <Board id='board-1' className='board' dots={dots}></Board>
+        <div className='dataForm'>
+          <button className='addBtn' onClick={addCard}>
+            add
+          </button>
+          <table>
+            <thead>
+              <tr>
+                <th>name</th>
+                <th>x-axis</th>
+                <th>y-axis</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {dots.map((dot, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <input
+                        type='text'
+                        name='name'
+                        value={dot.name}
+                        onChange={(e) => {
+                          dot.name = e.target.value;
+                          setRend((prev) => !prev);
+                        }}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type='number'
+                        name='x-axis'
+                        // value={dot.x_axis - 117}
+                        value={dot.x_axis}
+                        onChange={(e) => {
+                          if (e.target.value > -1 && e.target.value < 101)
+                            dot.x_axis = parseFloat(e.target.value);
+                          if (dots) localStorage.setItem("dots", [...dots]);
+                          setRend((prev) => !prev);
+                        }}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type='number'
+                        name='y-axis'
+                        // value={-252 - dot.y_axis}
+                        value={dot.y_axis}
+                        onChange={(e) => {
+                          if (e.target.value < 101 && e.target.value > -1)
+                            dot.y_axis = parseFloat(e.target.value);
+                          if (dots) localStorage.setItem("dots", [...dots]);
+                          setRend((prev) => !prev);
+                        }}
+                      />
+                    </td>
+                    <td
+                      className='deleteBtn'
+                      onClick={(id) => deleteCard(dot.id)}
+                    >
+                      delete
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,63 @@
+import { set } from "mongoose";
+import React, { useState, useEffect } from "react";
+
+function Card(props) {
+  // setHandleRef = ref => {
+  //   this.handleRef = ref;
+  // }
+  const [dragStartLeft, setDragStartLeft] = useState();
+  const [dragStartTop, setDragStartTop] = useState();
+  const [dragX, setDragX] = useState();
+  const [dragY, setDragY] = useState();
+  const [x_axis, setX_axis] = useState(props.x_axis);
+  const [y_axis, setY_axis] = useState(props.y_axis);
+  const [mouseUsed, setMouseUsed] = useState(false);
+
+  const [ref, setRef] = useState();
+  const initialiseDrag = (event) => {
+    const { target, clientX, clientY } = event;
+    const { offsetTop, offsetLeft } = target;
+    const { left, top } = ref.getBoundingClientRect();
+    setDragStartLeft(left - offsetLeft);
+    setDragStartTop(top - offsetTop);
+    setDragX(clientX);
+    setDragY(clientY);
+
+    window.addEventListener("mousemove", startDragging, false);
+    window.addEventListener("mouseup", stopDragging, false);
+  };
+
+  const startDragging = ({ screenX, screenY, clientX, clientY }) => {
+    setX_axis(screenX);
+    setY_axis(screenY);
+    setMouseUsed(true);
+  };
+
+  const stopDragging = () => {
+    window.removeEventListener("mousemove", startDragging, false);
+    window.removeEventListener("mouseup", stopDragging, false);
+  };
+  return (
+    <div
+      id={props.id}
+      className={props.className}
+      style={
+        !mouseUsed
+          ? {
+              top: 390 - parseFloat(y_axis) * 4 + 3,
+              left: parseFloat(x_axis) * 4 - 8,
+            }
+          : {
+              top: -250 + y_axis,
+              left: x_axis,
+            }
+      }
+      onMouseDown={initialiseDrag}
+      ref={setRef}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default Card;
